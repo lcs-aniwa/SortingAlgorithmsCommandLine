303487be1b200ffe2e6bfaf93db86a8056b99584
Niwa Ayana <aniwa@lcs-mba3092.lcs.on.ca>
2018-03-05 13:34:07 -0500

    Implemented Sorting Algorithm

--- SortingAlgorithmsCommandLine/main.swift
+++ SortingAlgorithmsCommandLine/main.swift
@@ -41,6 +41,45 @@ print("-----")
 
 // ----------- Write code to sort the array here -----------
 print("Now sorting the array...")
+// Invoke (use) the function above
+random(between: 1, and: 13)
+
+// Make a unsorted list
+var unsorted : [Int] = []
+var sorted : [Int] = []
+
+// Fill the list with values
+for _ in 1...13 {
+    unsorted.append ( random(between: 1, and: 50) )
+    
+}
+// keep reapiting untill there is not values in the unsorted
+
+while unsorted.count > 0 {
+    
+    //Add the last number of the unsorted array to sorted
+    sorted.insert(unsorted[unsorted.count-1], at: sorted.count)
+    
+    //Compare the new value to other values
+    for i in stride(from: sorted.count - 1, through: 1, by: -1) {
+        
+        //If the new number is smaller, compare it to the number one down in the list
+        if sorted[i] < sorted[i-1] {
+            //If the number is smaller, move it on the list
+            let temporaryValue = sorted[i]
+            sorted[i] = sorted[i-1]
+            sorted[i-1] = temporaryValue
+        }
+    }
+    
+    
+    sorted
+    
+    //Remove the last number from unsorted list
+    unsorted.remove(at: unsorted.count - 1)
+    
+    unsorted
+}
 
 // ----------- Final part of the program, show the sorted list -----------
 print("-----")
